3b311b295fd24a016ed0917af21f888607ce9e69
DanyPlaya <danila.lusuy@gmail.com>
2023-04-02 21:26:42 +0300

    добавил валидацию

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-
+import { body } from "express-validator";
+import { UserController } from "../controllers/user-controller.js";
+const userController = new UserController();
 export const userRouter = new Router();
 
-userRouter.post("/registrations");
-userRouter.post("login");
-userRouter.post("/logout");
-userRouter.get("/activate/:link");
-userRouter.get("/refresh");
-userRouter.get("/users");
+userRouter.post(
+  "/registration",
+  body("email").isEmail(),
+  body("password").isLength({ min: 3, max: 32 }),
+  userController.registration,
+);
+userRouter.post("/login", userController.login);
+userRouter.post("/logout", userController.logout);
+userRouter.get("/activate/:link", userController.activate);
+userRouter.get("/refresh", userController.refresh);
+userRouter.get("/users", userController.getUsers);
